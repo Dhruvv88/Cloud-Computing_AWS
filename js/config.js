aadf19285affa21755fd186784cf569147050bfa
Dhruvv88 <110650878+Dhruvv88@users.noreply.github.com>
2024-07-12 23:17:23 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_hqhl5cKNL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '63nj9n7ljjvrm9hberq4klubhp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://5s93ogj6x1.execute-api.us-west-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
